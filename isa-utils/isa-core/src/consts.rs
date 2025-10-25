a474748247897f629f8db28dc1cf9ec9834154f2
joha7809 <joha7809@gmail.com>
2025-10-25 23:46:19 +0200

    Remove old isa-utils folder in preparation for submodule

--- isa-utils/isa-core/src/consts.rs
+++
@@ -1,2 +0,0 @@
-pub const REGISTER_LIMIT: usize = 32; // 0..31
-pub const REGISTER_BIT: usize = 32;
--- isa-utils/isa-encoder/src/encoder.rs
+++
@@ -1,5 +0,0 @@
-use isa_core::{codec::EncodeError, traits::Encodable, types::ResolvedInstruction};
-
-pub fn encode_program(program: &[ResolvedInstruction]) -> Result<Vec<u32>, EncodeError> {
-    program.iter().map(|i| i.encode()).collect()
-}
