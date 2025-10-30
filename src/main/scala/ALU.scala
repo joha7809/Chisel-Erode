ff55686d63c23c4bd6c8d578e1671bc4975c63a7
joha7809 <joha7809@gmail.com>
2025-10-30 14:00:05 +0100

    Input types of ALU

--- src/main/scala/ALU.scala
+++ src/main/scala/ALU.scala
@@ -3,9 +3,15 @@ import chisel3.util._
 
 class ALU extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
+    // Define the module interface here (inputs/outputs)
+    val operand1 = Input(SInt(32.W))
+    val operand2 = Input(SInt(32.W))
+    val opcode = Input(UInt(4.W))
+    val result = Output(SInt(32.W))
+    val zero_falg = Output(Bool())
   })
 
-  //Implement this module here
+  // Implement this module here
 
 }
+
